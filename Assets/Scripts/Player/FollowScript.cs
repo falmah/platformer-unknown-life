0d8586f4962a61155e5738d811d7c5f77e6f6a97
Ivan Pavlyk <bernero789@gmail.com>
2020-11-05 15:14:34 +0200

    initial commit

---
+++ Assets/Scripts/Player/FollowScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowScript : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 offset;
+    public float delay;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetPos = player.position + offset;
+        Vector3 delayPos = Vector3.Lerp(transform.position, targetPos, delay * Time.deltaTime);
+        transform.position = delayPos;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
+    private float jumpTimeCounter;
+    private bool isGrounded;
+    private bool isJumping;
+    private bool positionChanged;
+
+    public float jumpTime;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    public float moveSpeed;
+    public float jumpForce;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void FixedUpdate() 
+    {
+        float direction = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
+        if (direction != 0)
+        {
+            var newScale = new Vector3(direction, 1, 1);
+            transform.localScale = newScale;
+        }
+        //cameraMoving();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+    
+    void cameraMoving()
+    {
+        float direction = Input.GetAxisRaw("Horizontal");
+        var pos = feetPos.position;
+        switch (direction) {
+            case -1:
+                if (!positionChanged)
+                    feetPos.position = new Vector3(pos.x + 20, pos.y, pos.z);
+                break;
+            case 0:
+                feetPos.position = new Vector3(pos.x - 20, pos.y, pos.z);
+                break;
+            case 1:
+                if(!positionChanged)
+                    feetPos.position = new Vector3(pos.x - 20, pos.y, pos.z);
+                break;
+
+        }     
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+    }
+
+    void Jump()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+        if (isGrounded && Input.GetKeyDown(KeyCode.W))
+        {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            if (jumpTimeCounter > 0 && isJumping)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            isJumping = false;
+        }
+    }
+}
+
